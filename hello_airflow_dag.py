ce448b93d668912420607074f4063abbfa3dc017
tryoo0607 <tryoo0607@gmail.com>
2025-12-12 22:04:07 +0900

    샘플 코드 문법 수정

--- hello_airflow_dag.py
+++ hello_airflow_dag.py
@@ -3,16 +3,16 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="hello_airflow",
-    start_date=datetime(2024, 1, 1),
-    schedule_interval=None,   # 수동 실행
-    catchup=False,
-    tags=["test"],
+        dag_id="hello_airflow",
+        start_date=datetime(2024, 1, 1),
+        schedule=None,     # ⭐ Airflow 3 필수
+        catchup=False,
+        tags=["test"],
 ) as dag:
 
     hello = BashOperator(
         task_id="say_hello",
-        bash_command="echo 'Hello Airflow'",
+        bash_command="echo 'Hello Airflow 3'",
     )
 
     hello
